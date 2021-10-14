7f61a25f3787f1fa9e1b30d48653f93602466d5e
kexianzheng <kexianzheng6852@gmail.com>
2021-10-14 10:20:19 -0400

    qiusushu

--- hello.go
+++ hello.go
@@ -3,28 +3,11 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
-	for i := 2; i <= 100; i++ {
-		flag := true //jilujshifouweisushu
-		for j := 2; j < i; j++ {
-			if i%j == 0 {
-				flag = false //
-				break
-			}
-		}
-		if flag { //==true
-			fmt.Println(i)
-		}
-	}
-=======
-	res := getSum()
-	fmt.Println("1-10dehe: ", res)
+	res1, res2 := rectangle(6, 4)
+	fmt.Println("zhouchang: ", res1, "mianji:", res2)
 }
-func getSum() int {
-	sum := 0
-	for i := 1; i <= 10; i++ {
-		sum += i
-	}
-	return sum
->>>>>>> qiusushu
+func rectangle(len, wid float64) (float64, float64) {
+	perimeter := (len + wid) * 2
+	area := len * wid
+	return perimeter, area
 }
